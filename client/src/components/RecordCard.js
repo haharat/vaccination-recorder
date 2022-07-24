3f2c3a7a87bca4f83aa27efc9513ba61c22529eb
haharat <xichen923@gmail.com>
2022-07-24 16:51:08 -0400

    Update RecordCard.js

--- client/src/components/RecordCard.js
+++ client/src/components/RecordCard.js
@@ -30,7 +30,7 @@ const RecordCard = () => {
         formData.append('file', file);
 
         try {
-            const res = await axios.post('http://localhost:5000/upload ', formData, {
+            const res = await axios.post('/upload', formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 },
